f3115a9b8ff678bf53d24fbcae33413d6e6b1ceb
Specialist17 <arocho.fernando.13@gmail.com>
2017-09-19 18:45:26 -0700

    protocol extensions

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -5,66 +5,58 @@ import PlaygroundSupport
 
 protocol Vehicle: CustomStringConvertible {
     var isRunning: Bool {get set}
-    
+    var make: String {get set}
+    var model: String {get set}
     mutating func start()
     mutating func turnOff()
 }
 
-struct SportsCar: Vehicle {
-    var isRunning: Bool = false
-    var description: String{
-        if isRunning {
-            return "Sports car already running"
-        } else {
-            return "Sports car turned off"
-        }
-    }
-    mutating func start() {
-        if isRunning {
-            print("Already started!")
-        } else {
-            isRunning = true
-            print("Vroom")
-        }
+extension Vehicle {
+    
+    var makeModel: String{
+        return "\(make) \(model)"
     }
     
+    mutating func start() {
+        if isRunning {
+            print("Started")
+        } else {
+            isRunning = true
+            print("\(self.description) fired up!")
+        }
+    }
     mutating func turnOff() {
         if isRunning {
             isRunning = false
-            print("Crick")
+            print("\(self.description) shut down!")
         } else {
-            print("Already dead!")
+            print("Dead")
         }
+    }
+}
+
+struct SportsCar: Vehicle {
+    var isRunning: Bool = false
+    var make: String
+    var model: String
+    var description: String{
+        return self.makeModel
     }
 }
 
 //Classes don't need the mutating keyword because they're a reference type, the properties are already mutable
 class SemiTruck: Vehicle {
     var isRunning: Bool = false
+    var make: String
+    var model: String
     var description: String {
-        if isRunning {
-            return "Semitruck already running"
-        } else {
-            return "Sports car turned off"
-        }
+        return self.makeModel
     }
     
-    func start() {
-        if isRunning {
-            print("Already started!")
-        } else {
-            isRunning = true
-            print("Rumble")
-        }
-    }
-    
-    func turnOff() {
-        if isRunning {
-            isRunning = false
-            print("Dead silence")
-        } else {
-            print("Already dead!")
-        }
+    init(isRunning: Bool, make: String, model: String) {
+        self.isRunning = isRunning
+        self.make = make
+        self.model = model
     }
     
     func blowAirHorn() {
@@ -73,8 +65,8 @@ class SemiTruck: Vehicle {
 }
 
 
-var miNegro = SportsCar(isRunning: true)
-var truck = SemiTruck()
+var miNegro = SportsCar(isRunning: true, make: "Audi", model: "R8")
+var truck = SemiTruck(isRunning: false, make: "Peterbuilt", model: "Verago")
 miNegro.start()
 miNegro.turnOff()
 
@@ -87,3 +79,11 @@ for vehicle in vehicleArray {
 }
 
 
+extension Double {
+    var dollarString: String {
+        return String(format: "$%.02f", self)
+    }
+}
+
+var pct = 32.15 * 0.17
+pct.dollarString
